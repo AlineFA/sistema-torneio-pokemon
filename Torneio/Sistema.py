f2dd356c829d8102799312bb7d578ae7339a2622
AlineFA <alinefasse@gmail.com>
2026-05-05 10:23:43 -0300

    Adicionando documentação na classe Tipo e o método __str__

--- Torneio/Sistema.py
+++ Torneio/Sistema.py
@@ -1,4 +1,5 @@
 class Tipo:
+    """Representa o tipo de um Pokémon ou de um golpe."""
 
     def __init__(self, nome, efetividade):
         self.__nome = nome
@@ -11,9 +12,21 @@ class Tipo:
         return self.__efetividade
 
     def multiplicador_efetividade(self, tipo_alvo):
+        """
+        Retorna o multiplicador de dano deste tipo contra o tipo alvo.
+        Se o tipo alvo não estiver definido, retorna 1 (neutro).
+        """
+
+        if tipo_alvo is None:
+            return 1
+
         nome_alvo = tipo_alvo.get_nome()
     
         if nome_alvo in self.__efetividade:
             return self.__efetividade[nome_alvo]
     
         return 1
+
+
+    def __str__(self):
+        return self.__nome
