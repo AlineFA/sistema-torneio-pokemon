2645d089d3312185113d28ef42075fbad2802e07
AlineFA <alinefasse@gmail.com>
2026-05-05 08:54:16 -0300

    Implementando método multiplicador_efetividade

--- Torneio/Sistema.py
+++ Torneio/Sistema.py
@@ -10,4 +10,10 @@ class Tipo:
     def get_efetividade(self):
         return self.__efetividade
 
+    def multiplicador_efetividade(self, tipo_alvo):
+        nome_alvo = tipo_alvo.get_nome()
     
+        if nome_alvo in self.__efetividade:
+            return self.__efetividade[nome_alvo]
+    
+        return 1
