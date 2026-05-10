ecdf6d6e7d89594632f55bdcb809d4c622216d30
AlineFA <alinefasse@gmail.com>
2026-05-10 20:11:38 -0300

    Adicionando a classe abstrata EfeitoStatus e um método getter de acesso

--- Torneio/Sistema.py
+++ Torneio/Sistema.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class Tipo:
     """Representa o tipo de um Pokémon ou de um golpe."""
 
@@ -33,83 +35,96 @@ class Tipo:
     
 
 class Pokemon:
-        """Representa um Pokémon no sistema."""
+    """Representa um Pokémon no sistema."""
 
-        def __init__(self, nome, tipo, hp, ataque, defesa, velocidade):
-            nivel = 50
+    def __init__(self, nome, tipo, hp, ataque, defesa, velocidade):
+        nivel = 50
 
-            self.__nome = nome[:15]
-            self.__tipo = tipo
+        self.__nome = nome[:15]
+        self.__tipo = tipo
 
-            vida_calculada = int(((2 * hp) * nivel) / 100) + nivel + 10
-            self.__vida_max = min(vida_calculada, 255)
+        vida_calculada = int(((2 * hp) * nivel) / 100) + nivel + 10
+        self.__vida_max = min(vida_calculada, 255)
+        self.__vida_atual = self.__vida_max
+
+        self.__ataque = min(int(((2 * ataque) * nivel) / 100) + 5, 255)
+        self.__defesa = min(int(((2 * defesa) * nivel) / 100) + 5, 255)
+        self.__velocidade = min(int(((2 * velocidade) * nivel) / 100) + 5, 255)
+
+        self.__golpes = []
+        self.__status = []
+
+    def get_nome(self):
+        return self.__nome
+        
+    def get_tipo(self):
+        return self.__tipo
+        
+    def get_vida_maxima(self):
+        return self.__vida_max
+        
+    def get_vida_atual(self):
+        return self.__vida_atual
+        
+    def get_ataque(self):
+        return self.__ataque 
+
+    def get_defesa(self):
+        return self.__defesa
+        
+    def get_velocidade(self):
+        return self.__velocidade
+        
+    def get_golpes(self):
+         return self.__golpes
+        
+    def get_status(self):
+        return self.__status
+        
+    def receber_dano(self, dano):
+        """Reduz a vida atual pelo dano recebido. A vida nunca fica abaixo de zero."""
+        self.__vida_atual -= dano
+        if self.__vida_atual < 0:
+            self.__vida_atual = 0
+
+    def receber_cura(self, cura):
+        """Adiciona na vida atual a cura recebida. A vida nunca fica acima do máximo."""
+        self.__vida_atual += cura
+        if self.__vida_atual > self.__vida_max:
             self.__vida_atual = self.__vida_max
 
-            self.__ataque = min(int(((2 * ataque) * nivel) / 100) + 5, 255)
-            self.__defesa = min(int(((2 * defesa) * nivel) / 100) + 5, 255)
-            self.__velocidade = min(int(((2 * velocidade) * nivel) / 100) + 5, 255)
-
-            self.__golpes = []
-            self.__status = []
-
-        def get_nome(self):
-            return self.__nome
-        
-        def get_tipo(self):
-            return self.__tipo
-        
-        def get_vida_maxima(self):
-            return self.__vida_max
-        
-        def get_vida_atual(self):
-            return self.__vida_atual
-        
-        def get_ataque(self):
-            return self.__ataque 
-
-        def get_defesa(self):
-            return self.__defesa
-        
-        def get_velocidade(self):
-            return self.__velocidade
-        
-        def get_golpes(self):
-            return self.__golpes
-        
-        def get_status(self):
-            return self.__status
-        
-        def receber_dano(self, dano):
-            """Reduz a vida atual pelo dano recebido. A vida nunca fica abaixo de zero."""
-            self.__vida_atual -= dano
-            if self.__vida_atual < 0:
-                self.__vida_atual = 0
-
-        def receber_cura(self, cura):
-            """Adiciona na vida atual a cura recebida. A vida nunca fica acima do máximo."""
-            self.__vida_atual += cura
-            if self.__vida_atual > self.__vida_max:
-                self.__vida_atual = self.__vida_max
-
-        def adicionar_golpe(self, golpe):
-            """Adiciona um golpe. A quantidade de golpes nunca é maior que 4."""
-            if len(self.__golpes) < 4:
-                self.__golpes.append(golpe)
+    def adicionar_golpe(self, golpe):
+        """Adiciona um golpe. A quantidade de golpes nunca é maior que 4."""
+        if len(self.__golpes) < 4:
+            self.__golpes.append(golpe)
          
-        def adicionar_status (self, status):
-            """Adiciona um status, mas somente se o Pokémon não estiver com o mesmo.""" 
-            if status not in self.__status:
+    def adicionar_status (self, status):
+        """Adiciona um status, mas somente se o Pokémon não estiver com o mesmo.""" 
+        if status not in self.__status:
                 self.__status.append(status)
 
-        def remover_status (self, status):
-            """Remove um status, mas somente se ele estiver aplicado ao Pokémon.""" 
-            if status in self.__status:
-                self.__status.remove(status)
+    def remover_status (self, status):
+        """Remove um status, mas somente se ele estiver aplicado ao Pokémon.""" 
+        if status in self.__status:
+            self.__status.remove(status)
        
-        def esta_desmaiado(self):
-            """Retorna True se o Pokémon está sem vida e fora de combate."""
-            return self.__vida_atual == 0
+    def esta_desmaiado(self):
+        """Retorna True se o Pokémon está sem vida e fora de combate."""
+        return self.__vida_atual == 0
         
-        def __str__(self):
-            return f"{self.__nome} | Tipo: {self.__tipo} | Vida Máxima: {self.__vida_max} | Vida atual {self.__vida_atual}"
+    def __str__(self):
+        return f"{self.__nome} | Tipo: {self.__tipo} | Vida Máxima: {self.__vida_max} | Vida atual {self.__vida_atual}"
 
+class EfeitoStatus(ABC):
+
+    """Classe abstrata que serve de base para os efeitos gerados pelos status, 
+    define o padrão que suas subclasses devem seguir usando o método aplicar()"""
+
+    def __init__(self, nome):
+        self.__nome = nome [:10]
+
+    def get_nome(self):
+        return self.__nome
+
+    
+    
