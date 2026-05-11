31becb6765b261c33683fc465b786d51013cc92e
AlineFA <alinefasse@gmail.com>
2026-05-11 10:18:23 -0300

    Organizando o código numa ordem que faça mais sentido

--- Torneio/Sistema.py
+++ Torneio/Sistema.py
@@ -114,6 +114,98 @@ class Golpe():
     def __str__(self):
         return f"Nome: {self.__nome} | Tipo: {self.__tipo} | Poder: {self.__poder} | Acuracia: {self.__acuracia} | Efeito: {self.__efeito} | Chance: {self.__chance}"
 
+                
+class Pokemon:
+    """Representa um Pokémon no sistema."""
+
+    def __init__(self, nome, tipo, hp, ataque, defesa, velocidade):
+        nivel = 50
+
+        self.__nome = nome[:15]
+        self.__tipo = tipo
+
+        vida_calculada = int(((2 * hp) * nivel) / 100) + nivel + 10
+        self.__vida_max = min(vida_calculada, 255)
+        self.__vida_atual = self.__vida_max
+
+        self.__ataque = min(int(((2 * ataque) * nivel) / 100) + 5, 255)
+        self.__defesa = min(int(((2 * defesa) * nivel) / 100) + 5, 255)
+        self.__velocidade = min(int(((2 * velocidade) * nivel) / 100) + 5, 255)
+
+        self.__golpes = []
+        self.__status = []
+
+    def get_nome(self):
+        return self.__nome
+        
+    def get_tipo(self):
+        return self.__tipo
+        
+    def get_vida_maxima(self):
+        return self.__vida_max
+        
+    def get_vida_atual(self):
+        return self.__vida_atual
+        
+    def get_ataque(self):
+        return self.__ataque 
+
+    def get_defesa(self):
+        return self.__defesa
+        
+    def get_velocidade(self):
+        return self.__velocidade
+        
+    def get_golpes(self):
+         return self.__golpes
+        
+    def get_status(self):
+        return self.__status
+        
+    def receber_dano(self, dano):
+        """Reduz a vida atual pelo dano recebido. A vida nunca fica abaixo de zero."""
+
+        self.__vida_atual -= dano
+        if self.__vida_atual < 0:
+            self.__vida_atual = 0
+
+    def receber_cura(self, cura):
+        """Adiciona na vida atual a cura recebida. A vida nunca fica acima do máximo."""
+
+        self.__vida_atual += cura
+        if self.__vida_atual > self.__vida_max:
+            self.__vida_atual = self.__vida_max
+
+    def adicionar_golpe(self, golpe):
+        """Adiciona um golpe. A quantidade de golpes nunca é maior que 4."""
+
+        if len(self.__golpes) < 4:
+            self.__golpes.append(golpe)
+         
+    def adicionar_status (self, status):
+        """Adiciona um status, mas somente se o Pokémon não estiver com o mesmo.""" 
+
+        if status not in self.__status:
+            self.__status.append(status)
+
+    def aplicar_status(self):
+        for efeito in self.__status:
+            efeito.aplicar(self)
+            
+    def remover_status (self, status):
+        """Remove um status, mas somente se ele estiver aplicado ao Pokémon.""" 
+
+        if status in self.__status:
+            self.__status.remove(status)
+       
+    def esta_desmaiado(self):
+        """Retorna True se o Pokémon está sem vida e fora de combate."""
+
+        return self.__vida_atual == 0
+        
+    def __str__(self):
+        return f"{self.__nome} | Tipo: {self.__tipo} | Vida Máxima: {self.__vida_max} | Vida atual {self.__vida_atual}"
+
 
 class Item(ABC):
     """Classe abstrata que serve como base para os itens utilizados nas batalhas. 
@@ -256,95 +348,3 @@ class AcaoItem(Acao):
 
     def executar(self, pokemon):
         pass
-
-                
-class Pokemon:
-    """Representa um Pokémon no sistema."""
-
-    def __init__(self, nome, tipo, hp, ataque, defesa, velocidade):
-        nivel = 50
-
-        self.__nome = nome[:15]
-        self.__tipo = tipo
-
-        vida_calculada = int(((2 * hp) * nivel) / 100) + nivel + 10
-        self.__vida_max = min(vida_calculada, 255)
-        self.__vida_atual = self.__vida_max
-
-        self.__ataque = min(int(((2 * ataque) * nivel) / 100) + 5, 255)
-        self.__defesa = min(int(((2 * defesa) * nivel) / 100) + 5, 255)
-        self.__velocidade = min(int(((2 * velocidade) * nivel) / 100) + 5, 255)
-
-        self.__golpes = []
-        self.__status = []
-
-    def get_nome(self):
-        return self.__nome
-        
-    def get_tipo(self):
-        return self.__tipo
-        
-    def get_vida_maxima(self):
-        return self.__vida_max
-        
-    def get_vida_atual(self):
-        return self.__vida_atual
-        
-    def get_ataque(self):
-        return self.__ataque 
-
-    def get_defesa(self):
-        return self.__defesa
-        
-    def get_velocidade(self):
-        return self.__velocidade
-        
-    def get_golpes(self):
-         return self.__golpes
-        
-    def get_status(self):
-        return self.__status
-        
-    def receber_dano(self, dano):
-        """Reduz a vida atual pelo dano recebido. A vida nunca fica abaixo de zero."""
-
-        self.__vida_atual -= dano
-        if self.__vida_atual < 0:
-            self.__vida_atual = 0
-
-    def receber_cura(self, cura):
-        """Adiciona na vida atual a cura recebida. A vida nunca fica acima do máximo."""
-
-        self.__vida_atual += cura
-        if self.__vida_atual > self.__vida_max:
-            self.__vida_atual = self.__vida_max
-
-    def adicionar_golpe(self, golpe):
-        """Adiciona um golpe. A quantidade de golpes nunca é maior que 4."""
-
-        if len(self.__golpes) < 4:
-            self.__golpes.append(golpe)
-         
-    def adicionar_status (self, status):
-        """Adiciona um status, mas somente se o Pokémon não estiver com o mesmo.""" 
-
-        if status not in self.__status:
-            self.__status.append(status)
-
-    def aplicar_status(self):
-        for efeito in self.__status:
-            efeito.aplicar(self)
-            
-    def remover_status (self, status):
-        """Remove um status, mas somente se ele estiver aplicado ao Pokémon.""" 
-
-        if status in self.__status:
-            self.__status.remove(status)
-       
-    def esta_desmaiado(self):
-        """Retorna True se o Pokémon está sem vida e fora de combate."""
-
-        return self.__vida_atual == 0
-        
-    def __str__(self):
-        return f"{self.__nome} | Tipo: {self.__tipo} | Vida Máxima: {self.__vida_max} | Vida atual {self.__vida_atual}"
