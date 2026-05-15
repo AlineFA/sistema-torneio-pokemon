0ff26a197354a4692bd486f9b10886dd77cf4fe6
AlineFA <alinefasse@gmail.com>
2026-05-15 10:49:22 -0300

    Iniciando o arquivo app

--- app.py
+++ app.py
@@ -12,3 +12,21 @@ tipos = {}
 golpes = {} 
 pokemons = {}
 treinadores = {}
+torneio_treinadores = []
+
+with open(arquivo_entrada) as arq:
+
+    for linha in arq:
+        partes = linha.split(":",1) #separa só no primeiro : e para
+        comando = partes[0].strip()
+
+        if comando == "add_type":
+            pass
+        elif comando == "add_move":
+            pass
+        elif comando == "add_pokemon":
+            pass
+        elif comando == "add_trainer":
+            pass
+        elif comando == "add_tournament":
+            pass
