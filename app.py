6b96eb147e032d8b3950672c18be32dde43eb349
AlineFA <alinefasse@gmail.com>
2026-05-16 17:12:59 -0300

    Implementa leitura e criação de tipos a partir do arquivo de entrada

--- app.py
+++ app.py
@@ -21,7 +21,25 @@ with open(arquivo_entrada) as arq:
         comando = partes[0].strip()
 
         if comando == "add_type":
-            pass
+            args = partes[1].strip()
+            info = args.split(",", 1)
+            nome_tipo = info[0].strip()
+            caminho_csv = info[1].strip()
+            
+            if nome_tipo in tipos:
+                erros.append(f"Tipo '{nome_tipo}' já cadastrado — ignorado.")
+            else:
+                with open(caminho_csv) as arq_csv:
+                    leitor = csv.DictReader(arq_csv)
+                    for linha_csv in leitor:
+                        if linha_csv["Attacking"] == nome_tipo:
+                            efetividade = {}
+                            for tipo_alvo, multiplicador in linha_csv.items():
+                                if tipo_alvo != "Attacking":
+                                    efetividade[tipo_alvo] = float(multiplicador)
+                tipos[nome_tipo] = Tipo(nome_tipo, efetividade)
+
+        
         elif comando == "add_move":
             pass
         elif comando == "add_pokemon":
